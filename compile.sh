467a0736e404ee96ad705595122f9235f015ce77
tiger0132 <4c5948@gmail.com>
2018-12-10 22:35:55 +0800

    20181210

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #! /bin/zsh
 
 cp $1$2 Temp/src$2
-g++ Temp/src$2 -o Temp/exec -std=c++98 -Wall -Wshadow -Wextra -g -fsanitize=undefined -fsanitize=address
-# g++ Temp/src$2 -o Temp/exec -std=c++98 -Wall -Wshadow -Wextra -g
+# g++ Temp/src$2 -o Temp/exec -std=c++98 -Wall -Wshadow -Wextra -g -fsanitize=undefined -fsanitize=address
+g++ Temp/src$2 -o Temp/exec -std=c++98 -Wall -Wshadow -Wextra -g
