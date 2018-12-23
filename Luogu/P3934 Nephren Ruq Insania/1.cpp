33968bc865d10ea40e8b32c38581aca94e198b88
tiger0132 <4c5948@gmail.com>
2018-12-23 12:55:55 +0800

    20181223

--- Luogu/P3934 Nephren Ruq Insania/1.cpp
+++ Luogu/P3934 Nephren Ruq Insania/1.cpp
@@ -14,26 +14,41 @@ namespace BIT {
 		for (int i = 1; i <= n; i++) add(i, t[i] - t[i-1]);
 	}
 }
-ll p_(ll x, ll y, ll z) {
-	if (z == 1 || x % z == 0) return 0;
-	x %= z; ll ret = 1;
+ll p_(ll x, ll y, ll z, int& f) {
+	if (z == 1 || x % z == 0) { f = 1; return 0; }
+	if (x >= z) x %= z, f = 1;
+	ll ret = 1;
 	for (; y; y >>= 1) {
 		if (y & 1) {
-			if ((ret *= x) >= z) ret %= z;
+			if ((ret *= x) >= z) f = 1, ret %= z;
 		}
-		if ((x *= x) >= z) x %= z;
+		if ((x *= x) >= z) f = 1, x %= z;
 	}
 	return ret;
 }
 
 int pr[M], np[M], phi[M], cnt;
 ll a[N];
-ll query(int l, int r, int p) {
+ll query(int l, int r, int p, int &f) {/*
+	for (int i = l+1, _p = p; i <= r; i++) {
+		_p = phi[_p];
+		if (_p == 1) r = i;
+	}
+	ll lans = a[r] = BIT::query(r);
+	int flag = 0;
+	for (int i = r-1; i >= l; i--) {
+		a[i] = BIT::query(i);
+		if (flag) lans += phi[p];
+		flag = 0;
+		lans = p_(a[i], lans, p, f);
+	}
+	return lans;*/
 	a[l] = BIT::query(l);
-	if (p == 1) return 1;
-	if (a[l] >= p) (a[l] %= p) += p;
 	if (l == r) return a[l];
-	return p_(a[l], query(l+1, r, phi[p]), p);
+	if (p == 1) return 0;
+	int _=0; ll tmp = query(l+1, r, phi[p], _);
+	tmp += _ * phi[p];
+	return p_(a[l], tmp, p, f);
 }
 
 int n, m, op, l, r, x;
@@ -54,15 +69,15 @@ signed main() {
 			}
 		}
 	}
-	scanf("%lld%lld", &n, &m);
+	scanf("%d%d", &n, &m);
 	for (int i = 1; i <= n; i++) scanf("%lld", a+i);
 	BIT::init(n, a);
 	for (int i = 1; i <= m; i++) {
-		scanf("%lld%lld%lld%lld", &op, &l, &r, &x);
+		scanf("%d%d%d%d", &op, &l, &r, &x);
 		if (op == 1) {
 			BIT::add(l, r, x);
 		} else {
-			printf("%lld\n", query(l, r, x) % x);
+			int _; printf("%lld\n", query(l, r, x, _) % x);
 		}
 	}
 }
