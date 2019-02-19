f1c5648dd98d47fac5f41b888f650929622df7bd
tiger0132 <4c5948@gmail.com>
2019-02-19 20:47:11 +0800

    :bug: Fixed 动态开点 ver. of LP3834 & Deleted some trash

--- Luogu/P3834 【模板】可持久化线段树 1（主席树）/2.cpp
+++ Luogu/P3834 【模板】可持久化线段树 1（主席树）/2.cpp
@@ -8,7 +8,7 @@ struct node {
 	node *lc, *rc;
 	int v;
 	void pushup() { v = lc->v + rc->v; }
-	node(node* lc_, node* rc_, int v_) : v(v_), lc(lc_), rc(rc_) {}
+	node(node* lc_, node* rc_, int v_) : lc(lc_), rc(rc_), v(v_) {}
 	node* insert(int l, int r, int x) {
 		int mid = (l + r) >> 1;
 		if (l == r) return new node(null, null, v + 1);
@@ -18,7 +18,7 @@ struct node {
 }* null = new node(0, 0, 0), *root[N];
 
 int n;
-int query(node* L, node* R, int x, int l = 1, int r = M) {
+int query(node* L, node* R, int x, int l = 0, int r = M) {
 	if (l == r) return l;
 	int sum = R->lc->v - L->lc->v, mid = (l + r) >> 1;
 	if (x <= sum)
@@ -27,20 +27,15 @@ int query(node* L, node* R, int x, int l = 1, int r = M) {
 		return query(L->rc, R->rc, x - sum, mid + 1, r);
 }
 
-int x, y, z, n$, m, a[N], b[N];
+int x, y, z, n$, m, a[N];
 int main() {
 	null->lc = null->rc = null;
 	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++) scanf("%d", a + i), b[i] = a[i];
-	std::sort(b + 1, b + n + 1);
-	n$ = std::unique(b + 1, b + n + 1) - b - 1;
+	for (int i = 1; i <= n; i++) scanf("%d", a + i);
 	root[0] = null;
-	for (int i = 1; i <= n; i++) {
-		a[i] = std::lower_bound(b + 1, b + n + 1, a[i]) - b;
-		root[i] = root[i - 1]->insert(1, M, a[i]);
-	}
+	for (int i = 1; i <= n; i++) root[i] = root[i - 1]->insert(0, M, a[i]);
 	while (m--) {
 		scanf("%d%d%d", &x, &y, &z);
-		printf("%d\n", b[query(root[x - 1], root[y], z)]);
+		printf("%d\n", query(root[x - 1], root[y], z));
 	}
 }
