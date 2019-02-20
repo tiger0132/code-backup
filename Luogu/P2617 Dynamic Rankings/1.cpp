e9fd223d333c678e42eef67caa1751317e28a74a
tiger0132 <4c5948@gmail.com>
2019-02-20 12:59:59 +0800

    :heavy_check_mark: Updated LP2617

--- Luogu/P2617 Dynamic Rankings/1.cpp
+++ Luogu/P2617 Dynamic Rankings/1.cpp
@@ -1,51 +1,101 @@
 #include <algorithm>
 #include <cstdio>
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-fstrict-overflow")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
 
-const int N = 1e5 + 51;
-extern struct node* null;
+const int N = 5e5 + 55;
+extern struct node *null, *newNode(node *lc, node *rc, int v);
 struct node {
 	node *lc, *rc;
 	int v;
-	void pushup() { v = lc->v + rc->v; }
-	node(node* lc_, node* rc_, int v_) : lc(lc_), rc(rc_), v(v_) {}
-	node* insert(int l, int r, int x, int o) {
+	node() {}
+	node(node *lc_, node *rc_, int v_) : lc(lc_), rc(rc_), v(v_) {}
+	node *insert(int l, int r, int x, int o) {
 		int mid = (l + r) >> 1;
-		if ((v += o) && l == r) return new node(null, null, v + 1);
-		if (x <= mid) return new node(lc->insert(l, mid, x, o), rc, v + 1);
-		return new node(lc, rc->insert(mid + 1, r, x, o), v + 1);
+		if (l == r) return newNode(null, null, v + o);
+		if (x <= mid) return newNode(lc->insert(l, mid, x, o), rc, v + o);
+		return newNode(lc, rc->insert(mid + 1, r, x, o), v + o);
 	}
-}* null = new node(0, 0, 0), *rt[N];
+} *null = new node(0, 0, 0);
+static node *rt[N], pool[N * 90];
+inline node *newNode(node *lc, node *rc, int v) {
+	static node *ptr = pool;
+	ptr->lc = lc, ptr->rc = rc, ptr->v = v;
+	return ptr++;
+}
 
-node *L[N], *R[N];
-int n, m, cl, cr;
+static node *L[N], *R[N];
+static int n, m, cl, cr, n$, i, j;
 inline int lowbit(int x) { return x & -x; }
-int query(int x, int l = 1, int r = n + m) {
+int query(int x, int l = 1, int r = n$) {
 	if (l == r) return l;
 	int sum = 0, mid = (l + r) >> 1;
-	for (int i = 1; i <= cr; i++) sum += R[i]->lc->v;
-	// printf("(%d)", sum);
-	for (int i = 1; i <= cl; i++) sum -= L[i]->lc->v;
-	// printf("[%d, %d]: %d\n", l, mid, sum);
+	for (i = 1; i <= cr; i++) sum += R[i]->lc->v;
+	for (i = 1; i <= cl; i++) sum -= L[i]->lc->v;
 	if (x <= sum) {
-		for (int i = 1; i <= cl; i++) L[i] = L[i]->lc;
-		for (int i = 1; i <= cr; i++) R[i] = R[i]->lc;
+		for (i = 1; i <= cl; i++) L[i] = L[i]->lc;
+		for (i = 1; i <= cr; i++) R[i] = R[i]->lc;
 		return query(x, l, mid);
 	} else {
-		for (int i = 1; i <= cl; i++) L[i] = L[i]->rc;
-		for (int i = 1; i <= cr; i++) R[i] = R[i]->rc;
+		for (i = 1; i <= cl; i++) L[i] = L[i]->rc;
+		for (i = 1; i <= cr; i++) R[i] = R[i]->rc;
 		return query(x - sum, mid + 1, r);
 	}
 }
-void add(int x, int o = 1) {
-	for (int i = x; i <= n; i += lowbit(i)) rt[i]->insert(1, n + m, x, o);
+inline void add(int x, int y, int o) {
+	for (i = x; i <= n; i += lowbit(i)) rt[i] = rt[i]->insert(1, n$, y, o);
 }
 
-int src[N], a[N], b[N], c[N], t[N], n$;
-char op[2];
+static int src[N], a[N], b[N], c[N], t[N];
+static char op[2];
 int main() {
+	register int i, j;
+	null->lc = null->rc = null;
 	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++) scanf("%d", src + i), t[++n$] = src[i];
-	for (int i = 1; i <= m; i++) {
+	for (i = 1; i <= n; i++) scanf("%d", src + i), t[++n$] = src[i];
+	for (i = 1; i <= m; i++) {
 		scanf("%s%d%d", op, a + i, b + i);
 		if (*op == 'Q')
 			scanf("%d", c + i);
@@ -54,20 +104,18 @@ int main() {
 	}
 	std::sort(t + 1, t + n$ + 1);
 	n$ = std::unique(t + 1, t + n$ + 1) - t - 1;
-	for (int i = 1; i <= n; i++) add(src[i] = std::lower_bound(t + 1, t + n$ + 1, src[i]) - t);
-
-	for (int i = 1; i <= m; i++) {
+	for (i = 1; i <= n; i++) rt[i] = null;
+	for (i = 1; i <= n; i++) add(i, src[i] = std::lower_bound(t + 1, t + n$ + 1, src[i]) - t, 1);
+	for (i = 1; i <= m; i++) {
 		if (c[i]) {
 			cl = cr = 0;
-			for (int j = a[i] - 1; j; j -= lowbit(j)) L[++cl] = rt[j];
-			for (int j = b[i]; j; j -= lowbit(j)) R[++cr] = rt[j];
+			for (j = a[i] - 1; j; j -= lowbit(j)) L[++cl] = rt[j];
+			for (j = b[i]; j; j -= lowbit(j)) R[++cr] = rt[j];
 			printf("%d\n", t[query(c[i])]);
 		} else {
 			b[i] = std::lower_bound(t + 1, t + n$ + 1, b[i]) - t;
-			add(src[a[i]], -1);
-			// printf("del %d\n", src[a[i]]);
-			add(src[a[i]] = b[i]);
-			// printf("add %d\n", src[a[i]]);
+			add(a[i], src[a[i]], -1);
+			add(a[i], src[a[i]] = b[i], 1);
 		}
 	}
 }
