7515dd4ef484b92fcc8b65b545d5746ef03f2caa
tiger0132 <4c5948@gmail.com>
2019-03-17 07:21:08 +0800

    :heavy_check_mark: Accepted Solution 2 of LP4238

--- Luogu/P4238 【模板】多项式求逆/1.cpp
+++ Luogu/P4238 【模板】多项式求逆/1.cpp
@@ -30,24 +30,24 @@ void ntt(int *a, int n, int x) {
 	for (int i = 0; i < n; ++i) a[i] = 1ll * a[i] * _ % P;
 }
 
-int n, a[N], b[N], c[N];
+int n, a[N], ans[N], b[N];
 void go(int x) {
-	if (x == 1) return void(b[0] = p(a[0], P - 2));
+	if (x == 1) return void(ans[0] = p(a[0], P - 2));
 	go((x + 1) >> 1);
 	lim = 1, l = 0;
 	while (lim < (x << 1)) lim <<= 1, l++;
 	for (int i = 1; i < lim; ++i) r[i] = (r[i >> 1] >> 1) | ((i & 1) << (l - 1));
-	for (int i = 0; i < x; ++i) c[i] = a[i];
-	for (int i = x; i < lim; ++i) c[i] = 0;
-	ntt(c, lim, 1), ntt(b, lim, 1);
-	for (int i = 0; i < lim; ++i) b[i] = (2 - 1ll * c[i] * b[i] % P + P) % P * b[i] % P;
-	ntt(b, lim, -1);
+	for (int i = 0; i < x; ++i) b[i] = a[i];
 	for (int i = x; i < lim; ++i) b[i] = 0;
+	ntt(ans, lim, 1), ntt(b, lim, 1);
+	for (int i = 0; i < lim; ++i) ans[i] = (2 - 1ll * b[i] * ans[i] % P + P) % P * ans[i] % P;
+	ntt(b, lim, -1);
+	for (int i = x; i < lim; ++i) ans[i] = 0;
 }
 
 int main() {
 	scanf("%d", &n);
 	for (int i = 0; i < n; ++i) scanf("%d", a + i);
 	go(n);
-	for (int i = 0; i < n; ++i) printf("%d ", b[i]);
+	for (int i = 0; i < n; ++i) printf("%d ", ans[i]);
 }
