3298b90961ff92b84e19beddb788f13602535888
tiger0132 <4c5948@gmail.com>
2019-03-17 06:26:34 +0800

    :bug: Fixed LP4238

--- Luogu/P4238 【模板】多项式求逆/1.cpp
+++ Luogu/P4238 【模板】多项式求逆/1.cpp
@@ -1,8 +1,7 @@
 #include <algorithm>
 #include <cstdio>
-#include <cstring>
 
-const int N = 5e5 + 55, G = 3, G_ = 332748118, P = 998244353;
+const int P = 998244353, G = 3, N = 3e6 + 63;
 
 int p(int x, int y) {
 	int ret = 1;
@@ -11,44 +10,44 @@ int p(int x, int y) {
 	return ret;
 }
 
-int r[N], lim, l;
-int ntt(int *a, int op) {
-	for (int i = 0; i < lim; i++)
+int r[N], l, lim;
+void ntt(int *a, int n, int x) {
+	for (int i = 0; i < n; ++i)
 		if (i < r[i]) std::swap(a[i], a[r[i]]);
-	for (int i = 1; i < lim; i <<= 1) {
-		int wn = p(G, (P - 1) / (i << 1));
-		for (int j = 0; j < lim; j += i << 1) {
-			int w = 1;
-			for (int k = 0; k < i; k++, w = 1ll * w * wn % P) {
-				int x = a[j + k], y = a[i + j + k];
-				a[j + k] = (x + y) % P;
-				a[i + j + k] = (x - y + P) % P;
+	for (int i = 1; i < n; i <<= 1) {
+		int gn = p(G, (P - 1) / (i << 1));
+		for (int j = 0; j < n; j += i << 1) {
+			int t1, t2, g = 1;
+			for (int k = 0; k < i; k++, g = 1ll * g * gn % P) {
+				t1 = a[j + k], t2 = 1ll * g * a[j + k + i] % P;
+				a[j + k] = (t1 + t2) % P, a[j + k + i] = (t1 - t2 + P) % P;
 			}
 		}
 	}
+	if (x == 1) return;
+	int _ = p(n, P - 2);
+	std::reverse(a + 1, a + n);
+	for (int i = 0; i < n; ++i) a[i] = 1ll * a[i] * _ % P;
 }
 
-int n, a[N], b[N], t[N];
+int n, a[N], b[N], c[N];
 void go(int x) {
 	if (x == 1) return void(b[0] = p(a[0], P - 2));
-	go((x + 1) / 2);
+	go((x + 1) >> 1);
 	lim = 1, l = 0;
-	while (lim < n * 2) lim <<= 1, l++;
-	for (int i = 0; i < lim; i++) r[i] = (r[i >> 1] >> 1) | ((i & 1) << (l - 1));
-	memset(t, 0, sizeof t);
-	memcpy(t, a, x << 2);
-	ntt(b, 1), ntt(t, 1);
-	for (int i = 0; i <= lim; i++) b[i] = (2 - 1ll * b[i] * t[i] % P + P) % P * b[i] % P;
-	ntt(b, -1);
-	int tmp = p(x, P - 2);
-	std::reverse(a + 1, a + n);
-	for (int i = 0; i < n; i++) a[i] = 1ll * a[i] * tmp % P;
-	for (int i = x; i < lim; i++) b[i] = 0;
+	while (lim < (x << 1)) lim <<= 1, l++;
+	for (int i = 1; i < lim; ++i) r[i] = (r[i >> 1] >> 1) | ((i & 1) << (l - 1));
+	for (int i = 0; i < x; ++i) c[i] = a[i];
+	for (int i = x; i < lim; ++i) c[i] = 0;
+	ntt(c, lim, 1), ntt(b, lim, 1);
+	for (int i = 0; i < lim; ++i) b[i] = (2 - 1ll * c[i] * b[i] % P + P) % P * b[i] % P;
+	ntt(b, lim, -1);
+	for (int i = x; i < lim; ++i) b[i] = 0;
 }
 
 int main() {
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) scanf("%d", a + i);
+	for (int i = 0; i < n; ++i) scanf("%d", a + i);
 	go(n);
-	for (int i = 0; i < n; i++) printf("%d ", b[i]);
+	for (int i = 0; i < n; ++i) printf("%d ", b[i]);
 }
