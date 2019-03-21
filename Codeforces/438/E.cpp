d7d0a8da59661cbfda6c199f9c07202dd6d816b8
tiger0132 <4c5948@gmail.com>
2019-03-21 21:56:59 +0800

    :heavy_check_mark: Accepted CF438E

--- Codeforces/438/E.cpp
+++ Codeforces/438/E.cpp
@@ -1,6 +1,5 @@
-// 被卡常，待更新
-
 #include <algorithm>
+#include <cmath>
 #include <cstdio>
 #include <cstring>
 
@@ -8,7 +7,7 @@ const int N = 6e5 + 56, P = 998244353, G = 3, GInv = 332748118;
 
 int p(int x, int y) {
 	int ret = 1;
-	for (; y; y >>= 1, (x = 1ll * x * x % P))
+	for (; y; y >>= 1, x = 1ll * x * x % P)
 		if (y & 1) ret = 1ll * ret * x % P;
 	return ret;
 }
@@ -18,7 +17,7 @@ void ntt(int *a, int op) {
 	for (int i = 0; i < lim; i++)
 		if (i < r[i]) std::swap(a[i], a[r[i]]);
 	for (int i = 1; i < lim; i <<= 1) {
-		int gn = p(op == 1 ? G : GInv, (P - 1) / (i << 1));
+		int gn = p(op == 1 ? G : GInv, (P - 1) / (i * 2));
 		for (int j = 0; j < lim; j += i * 2) {
 			int g = 1;
 			for (int k = 0; k < i; k++, g = 1ll * g * gn % P) {
@@ -41,17 +40,15 @@ void mul(int *a, int *b, int *ans, F f) {
 	ntt(ans, -1);
 }
 void init(int n) {
-	for (lim = 1, l = 0; lim <= n;) lim <<= 1, l++;
-	for (int i = 1; i < lim; i++) r[i] = (r[i >> 1] >> 1) | ((i & 1) << (l - 1));
+	for (lim = 1, l = 0; lim <= n;) lim *= 2, l++;
+	for (int i = 0; i < lim; i++) r[i] = (r[i >> 1] >> 1) | ((i & 1) << (l - 1));
 }
 void inv(int n, int *a, int *ans) {
 	static int b[N];
 	if (n == 1) return void(ans[0] = p(a[0], P - 2));
-	inv((n + 1) / 2, a, ans);
-	init(n * 2);
-	memcpy(b, a, n << 2);
-	memset(b + n, 0, (lim - n) << 2);
-	mul(b, ans, ans, [](int x, int y) { return (2 - 1ll * x * y % P + P) % P * y % P; });
+	inv((n + 1) / 2, a, ans), init(n * 2);
+	memcpy(b, a, n << 2), memset(b + n, 0, (lim - n) << 2);
+	mul(b, ans, ans, [](int x, int y) { return 1ll * y * ((2 - 1ll * x * y % P + P) % P) % P; });
 	memset(ans + n, 0, (lim - n) << 2);
 }
 void derivative(int n, int *a, int *ans) {
@@ -78,18 +75,21 @@ void exp(int n, int *a, int *ans) {
 	mul(ans, b, ans, [](int x, int y) { return 1ll * x * y % P; });
 	memset(ans + n, 0, (lim - n) << 2);
 }
+void sqrt(int n, int *a, int *ans) {
+	static int b[N];
+	ln(n, a, b);
+	for (int i = 0; i < n; i++) b[i] = b[i] * 499122177ll % P;
+	exp(n, b, ans);
+}
 
-int n, m, k = 1, x, a[N], b[N], c[N];
+int a[N], b[N], c[N];
+int n, m, k = 1, x;
 int main() {
 	scanf("%d%d", &n, &m);
+	for (int i = 1; i <= n; i++) scanf("%d", &x), a[x]++;
 	while (k <= m) k <<= 1;
-	while (n--) scanf("%d", &x), c[x]++;
-	for (int i = 1; i <= m; i++) {
-		if (!c[i]) continue;
-		for (int j = i; j <= k; j += i) {
-			a[j] = (a[j] + 1ll * c[i] * p(j / i, P - 2)) % P;
-		}
-	}
-	exp(k, a, b);
-	for (int i = 1; i <= m; i++) printf("%d\n", b[i]);
+	for (int i = 0; i < k; i++) a[i] = (P - 4 * a[i] + !i) % P;
+	sqrt(k, a, b), b[0] = (b[0] + 1) % P;
+	inv(k, b, c);
+	for (int i = 1; i <= m; i++) printf("%d\n", c[i] * 2ll % P);
 }
