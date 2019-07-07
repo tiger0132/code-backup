03881eb7b562c5afafd4fae2b4e1ba9bcf9fff38
tiger0132 <4c5948@gmail.com>
2019-07-08 07:16:00 +0800

    :bug: Fixed can't find header file in cwd

--- compile.sh
+++ compile.sh
@@ -5,8 +5,8 @@ cp "$1$2" "Temp/src$2"
 if [ $2x = ".cpp"x ]; then
 	# g++ Temp/src$2 -o Temp/exec -std=c++98 -Wall -Wshadow -Wextra -g
 	# g++ Temp/src$2 -o Temp/exec -std=c++17 -Wall -Wshadow -Wextra -g
-	g++ Temp/src$2 -o Temp/exec -std=c++11 -Wall -Wshadow -Wextra -g
+	g++ Temp/src$2 -o Temp/exec -std=c++11 -Wall -Wshadow -Wextra -g $3
 elif [ $2x = ".c"x ]; then
-	gcc Temp/src$2 -o Temp/exec -std=c99 -Wall -Wshadow -Wextra -g -lm
+	gcc Temp/src$2 -o Temp/exec -std=c99 -Wall -Wshadow -Wextra -g -lm $3
 	# gcc Temp/src$2 -o Temp/exec -std=c99 -Wall -Wshadow -Wextra -g -fsanitize=undefined -fsanitize=address
 fi
