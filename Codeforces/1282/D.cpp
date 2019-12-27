3cb425c1df9c2d4523cb78688bc8f748c5560bc2
tiger0132 <4c5948@gmail.com>
2019-12-27 13:53:03 +0800

    :heavy_check_mark: Accepted CF1282D

--- Codeforces/1282/D.cpp
+++ Codeforces/1282/D.cpp
@@ -16,26 +16,21 @@ int n, s0, tmp, A, B;
 int main() {
 	for (int i = 0; i < 300; i++) s[i] = 'a';
 	for (int i = 0; i < 300; i++) b[i] = 'b';
-	s0 = A = 300 - qry(b);
-	if (A == 300) return 0;
-	B = 300 - qry(s);
+	s0 = B = 300 - qry(b);
 	if (B == 300) return 0;
+	A = 300 - qry(s);
+	if (A == 300) return 0;
 	n = B + A;
-	s[n + 1] = 0;
+	s[n] = 0;
 	for (int i = 0; i < n - 1; i++) {
 		s[i] = 'b';
 		tmp = qry(s);
 		if (tmp == -1) return 0;
-		if (tmp == 1 && n < 300) {
-			s[n] = 0;
-			qry(s);
-			return 0;
-		}
 		if (!tmp) return 0;
 		if (tmp >= s0) s[i] = 'a';
 		s0 = tmp;
 		(s[i] == 'a' ? A : B)--;
 	}
-	s[n - 1] = A ? 'a' : 'b', s[n] = 0;
-	qry(s);
+	s[n - 1] = A ? 'a' : 'b';
+	return qry(s);
 }
