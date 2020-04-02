609d05fa0263e1302d553e051b2e83dc8fc88f29
tiger0132 <linkcutcactus@gmail.com>
2020-04-03 00:11:39 +0800

    :heavy_check_mark: Accepted CF1207G

---
+++ Codeforces/1000~1999/1207/G.cpp
@@ -0,0 +1,310 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <map>
+#include <queue>
+#include <vector>
+
+#pragma region IO
+namespace IO {
+#ifdef __unix__
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
+struct in_t {
+#ifdef LOCAL_QWQ
+	inline in_t() {}
+	inline in_t(const char *path) { freopen(path, "r", stdin); }
+	inline char gc() { return getchar(); }
+#elif __unix__
+	struct stat st;
+	char *ptr;
+	int fd;
+	inline in_t() {
+		fd = fileno(stdin), fstat(fd, &st),
+		ptr = (char *)mmap(0, st.st_size, 1, 2, fd, 0);
+	}
+	inline in_t(const char *path) {
+		fd = open(path, O_RDONLY), fstat(fd, &st),
+		ptr = (char *)mmap(0, st.st_size, 1, 2, fd, 0);
+	}
+	inline char gc() { return *ptr++; }
+#else
+	static const int SZ = 1 << 20;
+	char buf[SZ], *s, *t;
+	inline in_t() {}
+	inline in_t(const char *path) { freopen(path, "r", stdin); }
+	inline char gc() {
+		if (s == t) t = (s = buf) + fread(buf, 1, SZ, stdin);
+		return s == t ? ' ' : *s++;
+	}
+#endif
+	template <typename T>
+	void readU(T &x) {
+		x = 0;
+		char c = gc();
+		while (c < '0') c = gc();
+		for (; c >= '0'; c = gc()) x = x * 10 + (c & 15);
+	}
+	template <typename T>
+	void readI(T &x) {
+		x = 0;
+		bool f = 0;
+		char c = gc();
+		for (; c < '0'; c = gc())
+			if (c == '-') f = 1;
+		for (; c >= '0'; c = gc()) x = x * 10 + (c & 15);
+		if (f) x = -x;
+	}
+	template <typename T>
+	void readF(T &x) {
+		T tmp = 1;
+		bool f = 0;
+		char c = gc();
+		x = 0;
+		for (; c < '0'; c = gc())
+			if (c == '-') f = 1;
+		for (; c >= '0'; c = gc()) x = x * 10 + (c & 15);
+		if (c == '.')
+			for (c = gc(); c >= '0'; c = gc()) tmp /= 10, x += tmp * (c & 15);
+		if (f) x = -x;
+	}
+
+#define READ_DEF(type, func) \
+	in_t &operator>>(type &x) { return func(x), *this; };
+#ifdef __x86_64__
+#define READ_DEF_X64(type, func) READ_DEF(type, func)
+#else
+#define READ_DEF_X64(type, func)
+#endif
+
+	READ_DEF(signed short, readI);
+	READ_DEF(signed int, readI);
+	READ_DEF(signed long, readI);
+	READ_DEF(signed long long, readI);
+	READ_DEF_X64(signed __int128, readI);
+
+	READ_DEF(unsigned short, readU);
+	READ_DEF(unsigned int, readU);
+	READ_DEF(unsigned long, readU);
+	READ_DEF(unsigned long long, readU);
+	READ_DEF_X64(unsigned __int128, readU);
+
+	READ_DEF(float, readF);
+	READ_DEF(double, readF);
+	READ_DEF(__float128, readF);
+
+	in_t &operator>>(char &c) {
+		for (c = gc(); c <= ' ';) c = gc();
+		return *this;
+	}
+	in_t &operator>>(char *s) {
+		char c;
+		for (c = gc(); c <= ' ';) c = gc();
+		for (; c > ' '; c = gc()) *s++ = c;
+		return *s = 0, *this;
+	}
+#ifdef _STRINGFWD_H
+	in_t &operator>>(std::string &s) {
+		char c;
+		s.clear();
+		for (c = gc(); c <= ' ';) c = gc();
+		for (; c > ' '; c = gc()) s += c;
+		return *this;
+	}
+#endif
+};
+
+struct out_t {
+#ifdef LOCAL_QWQ
+	inline out_t() { setvbuf(stdout, 0, _IONBF, 0); }
+	inline out_t(const char *path) {
+		freopen(path, "w", stdout);
+		setvbuf(stdout, 0, _IONBF, 0);
+	}
+	inline void flush() {}
+	inline void pc(char c) { putchar(c); }
+#else
+	static const int SZ = 1 << 20;
+	char buf[SZ], *s = buf, *t = buf + SZ;
+	inline out_t() { setvbuf(stdout, 0, _IONBF, 0); }
+	inline out_t(const char *path) {
+		freopen(path, "w", stdout);
+		setvbuf(stdout, 0, _IONBF, 0);
+	}
+	inline void flush() { fwrite(buf, 1, s - buf, stdout); }
+	inline void pc(char c) {
+		if (s == t) flush(), s = buf;
+		*s++ = c;
+	}
+	inline ~out_t() { flush(); }
+#endif
+	template <typename T>
+	void outU(const T &x) {
+		if (x > 9) outU(x / 10);
+		pc(x % 10 + '0');
+	}
+	template <typename T>
+	void outI(const T &x) {
+		if (x < 0) pc('-');
+		outU(x);
+	}
+
+#define OUT_DEF(type, func) \
+	out_t &operator<<(type x) { return func(x), *this; };
+#ifdef __x86_64__
+#define OUT_DEF_X64(type, func) OUT_DEF(type, func)
+#else
+#define OUT_DEF_X64(type, func)
+#endif
+
+	OUT_DEF(signed short, outI);
+	OUT_DEF(signed int, outI);
+	OUT_DEF(signed long, outI);
+	OUT_DEF(signed long long, outI);
+	OUT_DEF_X64(signed __int128, outI);
+
+	OUT_DEF(unsigned short, outU);
+	OUT_DEF(unsigned int, outU);
+	OUT_DEF(unsigned long, outU);
+	OUT_DEF(unsigned long long, outU);
+	OUT_DEF_X64(unsigned __int128, outU);
+
+	out_t &operator<<(char c) { return pc(c), *this; }
+	out_t &operator<<(char *s) {
+		while (*s) pc(*s++);
+		return *this;
+	}
+};
+}  // namespace IO
+using namespace IO;
+#pragma endregion
+
+in_t in;
+out_t out;
+
+const int N = 3e6 + 63;
+
+struct BIT {
+	int tr[N], n;
+	void init(int nn) { n = nn; }
+	void add(int i, int x) {
+		for (i++; i <= n + 1; i += i & -i) i[tr] += x;
+	}
+	int qry(int i) {
+		int r = 0;
+		for (i++; i; i -= i & -i) r += i[tr];
+		return r;
+	}
+	int qry(int l, int r) { return qry(r) - qry(l - 1); }
+};
+
+struct T {
+	std::vector<int> g[N];
+	void operator()(int x, int y) { x[g].push_back(y); }
+
+	int dfn[N], sz[N], idx;
+	BIT tr;
+	void init(int n) { tr.init(n); }
+	void dfs(int x) {
+		x[dfn] = ++idx, x[sz] = 1;
+		for (int nx : x[g]) dfs(nx), x[sz] += nx[sz];
+	}
+	void add(int x, int y) { tr.add(x[dfn], y); }
+	int qry(int x) { return tr.qry(x[dfn], x[dfn] + x[sz] - 1); }
+};
+
+struct SAM {
+	std::map<int, int> tr[N];
+	int link[N], len[N], nc;
+	SAM() { nc = 1; }
+	int ins(int last, int c) {
+		int cur = ++nc, p = last, q, cl;
+		cur[len] = p[len] + 1;
+		for (; p && !(q = p[tr][c]); p = p[link]) p[tr][c] = cur;
+		if (!p)
+			cur[link] = 1;
+		else if (p[len] + 1 == q[len])
+			cur[link] = q;
+		else {
+			cl = ++nc, cl[len] = p[len] + 1, cl[link] = q[link];
+			cl[tr] = q[tr];
+			for (; p && p[tr][c] == q; p = p[link]) p[tr][c] = cl;
+			q[link] = cur[link] = cl;
+		}
+		return cur;
+	}
+	void build(T &t) {
+		t.init(nc);
+		for (int i = 2; i <= nc; i++) t(i[link], i);
+		t.dfs(1);
+	}
+	int operator()(char *s) {
+		int x = 1;
+		while (*s) x = x[tr][*s++ - 97];
+		return x;
+	}
+};
+
+const int M = 4e5 + 54;
+struct Trie {
+	int ch[M][26], p[M], pos[M], v[M], nc;
+	Trie() { nc = 1; }
+	int operator()(int x, int c) {
+		if (x[ch][c]) return x[ch][c];
+		return nc++, (nc[p] = x)[ch][nc[v] = c] = nc;
+	}
+	void build(SAM &sam) {
+		std::queue<int> q;
+		for (int i = 0; i < 26; i++)
+			if (1 [ch][i]) q.push(1 [ch][i]);
+		for (1 [pos] = 1; !q.empty(); q.pop()) {
+			int x = q.front();
+			x[pos] = sam.ins(x[p][pos], x[v]);
+			for (int i = 0; i < 26; i++)
+				if (x[ch][i]) q.push(x[ch][i]);
+		}
+	}
+} trie;
+
+int pos[N], ans[N], q[N];
+std::vector<int> id[N];
+SAM sam;
+T tree;
+void dfs(int x) {
+	tree.add(x[trie.pos], 1);
+	for (int i : x[id]) i[ans] = tree.qry(i[q]);
+	for (int i = 0; i < 26; i++)
+		if (x[trie.ch][i]) dfs(x[trie.ch][i]);
+	tree.add(x[trie.pos], -1);
+}
+
+int n, op, x, p;
+char c, s[N];
+int main() {
+	in >> n;
+	0 [pos] = 1;
+	for (int i = 1; i <= n; i++) {
+		in >> op;
+		if (op == 1)
+			x = 0;
+		else
+			in >> x;
+		in >> c;
+		i[pos] = trie(x[pos], c - 97);
+	}
+	trie.build(sam);
+	sam.build(tree);
+	in >> n;
+	for (int i = 1; i <= n; i++) {
+		in >> x >> s;
+		i[q] = sam(s);
+		x[pos][id].push_back(i);
+	}
+	dfs(1);
+	for (int i = 1; i <= n; i++) out << i[ans] << '\n';
+}
