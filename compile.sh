57eac9c560aa2eda0ca78b3a9a5fe067a6040d0a
tiger0132 <linkcutcactus@gmail.com>
2020-09-17 09:06:35 +0800

    chore

--- compile.sh
+++ compile.sh
@@ -1,12 +1,11 @@
 #! /bin/zsh
 
-echo -e "\033c"
-cp "$1$2" "Temp/src$2"
+# echo -e "\033c"
 if [ $2x = ".cpp"x ]; then
 	# g++ Temp/src$2 -o Temp/exec -std=c++98 -Wall -Wshadow -Wextra -g
 	# g++ Temp/src$2 -o Temp/exec -std=c++17 -Wall -Wshadow -Wextra -g
-	g++ "Temp/src$2" -o Temp/exec -std=c++14 -Wall -Wshadow -Wextra -g -fsanitize=undefined -I/home/tiger0132/code-backup/Temp $3 -lquadmath -DLOCAL
+	g++ "$1$2" -o Temp/exec -std=c++17 -Wall -Wshadow -Wextra -g -fsanitize=undefined -I/home/tiger0132/code-backup/Temp $3 -lquadmath -DLOCAL_QWQ
 elif [ $2x = ".c"x ]; then
-	gcc "Temp/src$2" -o Temp/exec -std=c99 -Wall -Wshadow -Wextra -g -lm $3
+	gcc "$1$2" -o Temp/exec -std=c99 -Wall -Wshadow -Wextra -g -lm $3
 	# gcc Temp/src$2 -o Temp/exec -std=c99 -Wall -Wshadow -Wextra -g -fsanitize=undefined -fsanitize=address
 fi
